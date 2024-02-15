4890fd9775a55aa335bd2a923159713a24284993
Jade Piat <jade.piat99@gmail.com>
2024-02-15 14:47:39 -0800

    update randoms

--- randoms.py
+++ randoms.py
@@ -1,17 +1,19 @@
 import sys
+sys.path.append('/global/homes/j/jpiat/')
 sys.path.append('/global/homes/j/jpiat/shared_code/abacus_mocks/')
 
+import numpy as np
 from astropy.io import fits
 from astropy.table import vstack,Table
+from Relativistic_effects.gravitational_redshift import z_grav
 from hodpy.cosmology import CosmologyAbacus
+from hodpy.k_correction import GAMA_KCorrection
+from hodpy import lookup
 cosmo = CosmologyAbacus(0)  #c000 cosmology
+kcorr_r = GAMA_KCorrection(cosmo, k_corr_file=lookup.kcorr_file, cubic_interpolation=True)
 
-path_in = '/global/cfs/cdirs/desi/cosmosim/SecondGenMocks/AbacusSummit/CutSky/BGS/v0.1/z0.200/'
-path_out = '/pscratch/sd/j/jpiat/Abacus/Ab_c000_ph006/z0.200/Split/BGS/'
 
-n_randoms = 10
-
-def main(path_in, path_out, z_max):
+def main(path_in, path_out, n_randoms, space, z_max, m_lim):
 
     for i in range(1,n_randoms+1):
         
@@ -20,28 +22,41 @@ def main(path_in, path_out, z_max):
         sky = fits.open(input_file)
         data = Table(sky[1].data)
         sky.close()
+    
+        cond = (data['R_MAG_APP']<=m_lim) & (data['Z']<=z_max)
                            
-        data = data[data['Z_COSMO']<=z_max]
+        data = data[cond]
         
-        data['DIST'] = cosmo.comoving_distance(data['Z_COSMO'])
+        if space == 'real':
+        
+            data['COM_DIST'] = cosmo.comoving_distance(data['Z_COSMO'])
+            
+        else:
+            
+            data['COM_DIST'] = cosmo.comoving_distance(data['Z'])
         
         if i == 1:
             
-            table = data['RA','DEC','DIST']
+            table = data['Z','RA','DEC','COM_DIST']
             
             continue
         
-        table = vstack([table,data['RA','DEC','DIST']])
+        table = vstack([table,data['Z','RA','DEC','COM_DIST']])
         
-    output_file = path_out+'randoms_'+str(n_randoms)+'_cutsky_zmax'+str(z_max)+'.fits'
+    output_file = path_out+'randoms_'+str(n_randoms)+'_cutsky_'+space+'_zmax'+str(z_max)+'.fits'
         
-    table.write(output_file,format='fits')
+    table.write(output_file,format='fits',overwrite=True)
 
 
 
 if __name__ == "__main__":
     
-    main(path_in, path_out, 0.5)
+    path_in = '/global/cfs/cdirs/desi/cosmosim/SecondGenMocks/AbacusSummit/CutSky/BGS/v0.1/z0.200/'
+    path_out = '/pscratch/sd/j/jpiat/Abacus/Ab_c000_ph006/z0.200/Split/BGS/'
+
+    n_randoms = 10
+    
+    main(path_in, path_out, n_randoms, 'grav', 0.5, 19.5)
     
     
     
