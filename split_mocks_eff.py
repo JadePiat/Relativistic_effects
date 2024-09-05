c0d517c611c65fef33bda201dffc4b4bc3e0cea2
Jade Piat <jade.piat99@gmail.com>
2024-09-05 10:39:14 -0700

    projected corrfunc tests

--- split_mocks_eff.py
+++ split_mocks_eff.py
@@ -3,6 +3,7 @@ sys.path.append('/global/homes/j/jpiat/shared_code/abacus_mocks/')
 
 import numpy as np
 from astropy.io import fits
+from astropy.table import Table
 import matplotlib.pyplot as plt
 from hodpy.k_correction import GAMA_KCorrection
 from hodpy import lookup
@@ -14,44 +15,115 @@ cosmo = CosmologyAbacus(0)  #c000 cosmology
 kcorr_r = GAMA_KCorrection(cosmo, k_corr_file=lookup.kcorr_file, cubic_interpolation=True)
 
 
-def apparent_mag(M,z,g_r,Q=0.8):
+def apparent_mag(M,z,g_r,Q=0.8,z0=0.1):
     
     K = kcorr_r.apparent_magnitude(M, z, g_r)
-    EC = Q*(z - 0.1)
+    EC = Q*(z - z0)
     
     return K - EC
 
 
-def write_file(ra,dec,dist,output_file):
-    
-    c1 = fits.Column(name='RA', format='E', array=ra)
-    c2 = fits.Column(name='DEC', format='E', array=dec)
-    c3 = fits.Column(name='COM_DIST', format='E', array=dist)
-    
-    t = fits.BinTableHDU.from_columns([c1, c2, c3])
-    t.writeto(output_file,overwrite=True)
-    
+def distances(data):
 
-def split_mag_eff(input_file, space, m_lim, z_max, n_bins, cut_bright, cut_faint, output_path):
+    data['DIST_COM'] = cosmo.comoving_distance(data['Z_COSMO'])
+    data['DIST_RSD'] = cosmo.comoving_distance(data['Z_RSD'])
+    data['DIST_RSD+GR'] = cosmo.comoving_distance(data['Z_RSD+GR'])
+
+
+def write_file(data,output_file):
     
-    sky = fits.open(input_file)
-    data = sky[1].data
-    sky.close()
+    t = data['RA','DEC','DIST_COM','DIST_RSD','DIST_RSD+GR']
+    t.write(output_file,format='fits',overwrite=True)
+
+
+def magnitude_split(input_file, m_lim, z_max, n_bins, cut_bright, cut_faint, output_path):
+    
+    cat = fits.open(input_file)
+    data = Table(cat[1].data)
+    cat.close()
     
     M = data['R_MAG_ABS']
     g_r = data['G_R_REST']
+    z_cos = data['Z_COSMO']    
+    
+    phi = data['Pot A Soft 7']
+    phi0 = phi[z_cos==np.min(z_cos)]
+    
+    z = data['Z']
+    z_tot = z_grav(z_cos, z, phi, phi0)
+    
+    data.columns['Z'].name = 'Z_RSD'
+    data['Z_RSD+GR'] = z_tot
+    
+    m = apparent_mag(M,z_tot,g_r)
+    
+    cond = (np.isnan(m)==False)*(m<=m_lim)*(z_tot<=z_max)
+    m = m[cond]
+    z_tot = z_tot[cond]
+    
+    z_bins = np.linspace(0, z_max, n_bins+1)
+    
+    z_means = np.zeros(n_bins)
+    m_cuts_b = np.zeros(n_bins)
+    m_cuts_f = np.zeros(n_bins)
+    
+    for i in range(n_bins):
+        if i==(n_bins-1):
+            inside = (z_tot>=z_bins[i])*(z_tot<=z_bins[i+1])
+        else:
+            inside = (z_tot>=z_bins[i])*(z_tot<z_bins[i+1])
+        
+        z_means[i] = np.mean(z_tot[inside])
+        
+        m_cuts_b[i] = np.percentile(m[inside],cut_bright)
+        m_cuts_f[i] = np.percentile(m[inside],100-cut_faint)
+    
+    m_interp_b = CubicSpline(z_means, m_cuts_b, extrapolate=True)
+    m_interp_f = CubicSpline(z_means, m_cuts_f, extrapolate=True)
+    
+    cond_b = (m<m_interp_b(z_tot))
+    cond_f = (m>=m_interp_f(z_tot))
+    
+    data_b = data[cond][cond_b]
+    data_f = data[cond][cond_f]
+    
+    distances(data_b)
+    distances(data_f)
+    
+    output_file = output_path+f'cutsky_zmax{z_max:.1f}_m{m_lim:.1f}'
+    output_b = output_file+f'_bright_{cut_bright:d}.fits'
+    output_f = output_file+f'_faint_{cut_faint:d}.fits'
+    
+    write_file(data_b,output_b)
+    write_file(data_f,output_f)
+    
+    
+
+
+
+
+def eff_magnification_bias(input_file, space, m_lim, z_max, n_bins, cut_bright, cut_faint, output_path):
+    
+    cat = fits.open(input_file)
+    data = cat[1].data
+    cat.close()
+    
+    M = data['R_MAG_ABS']
+    g_r = data['G_R_REST']
+    
     ra = data['RA']
     dec = data['DEC']
     
-    z_cos = data['Z_COSMO']    
-    z = data['Z']
     phi = data['Pot A Soft 7']
     phi0 = phi[z_cos==np.min(z_cos)]
+    
+    z_cos = data['Z_COSMO']    
+    z = data['Z']
     z_tot = z_grav(z_cos, z, phi, phi0)
     
     m = apparent_mag(M,z_tot,g_r)
     
-    cond = (np.isnan(m)==False)*(m<=m_lim)*(z<=z_max)
+    cond = (np.isnan(m)==False)*(m<=m_lim)*(z_tot<=z_max)
     m = m[cond]
     M = M[cond]
     z_g = z_tot[cond]
@@ -146,8 +218,8 @@ def split_mag_eff(input_file, space, m_lim, z_max, n_bins, cut_bright, cut_faint
     m_interp_b = CubicSpline(z_means, m_cuts_b, extrapolate=True)
     m_interp_f = CubicSpline(z_means, m_cuts_f, extrapolate=True)
     
-    cond_b = (m<=m_interp_b(z_g))
-    cond_f = (m>m_interp_f(z_g))    
+    cond_b = (m<m_interp_b(z_g))
+    cond_f = (m>=m_interp_f(z_g))    
     
     m_b = m[cond_b]
     m_f = m[cond_f]
